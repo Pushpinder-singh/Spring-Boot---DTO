8bcd2648cb128a4952a2f9164ba9747a2dec2b3a
pushpinder singh <pushpinder.1990singh@gmail.com>
2025-06-04 10:00:32 -0400

    User and Office Locations

---
+++ Spring-Boot-DTO/src/test/java/com/pushp/Spring/Boot_DTO/SpringBootDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushp.Spring.Boot_DTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
